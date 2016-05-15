1272cc1c5c2bc7bc1338e1826ff6ee0b1e304ea0
Carol <Carol>
2016-05-15 16:22:59 -0400

    add initial files

---
+++ WebApplication3/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebApplication3
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters
+        {
+            Filters.Add(new HandleErrorAttribute());
+        }
+
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+        routes.IgnoreRoute("{resource".axd/{*pathInfo)
+
+    }
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
